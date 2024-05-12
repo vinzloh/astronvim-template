4e63cdcd8526eec1bb7cae720800ae8bfdb39538
Vince Loh <5501845+vinzloh@users.noreply.github.com>
2024-05-13 01:52:37 +0800

    typescript react template

---
+++ lua/community.lua
@@ -0,0 +1,22 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.recipes.vscode-icons" },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup()
+      vim.cmd.colorscheme "night-owl"
+    end,
+  },
+  -- { import = "astrocommunity.colorscheme.night-owl" },
+  -- import/override with your plugins folder
+}
